c77b057b760a5cf24388c571bb8005fb75460c31
fank <locke_fank@126.com>
2019-09-25 17:14:27 +0800

    add TestClass activity

--- app/src/main/java/com/example/ideavcs/TestClass.kt
+++ app/src/main/java/com/example/ideavcs/TestClass.kt
@@ -1,4 +1,11 @@
 package com.example.ideavcs
 
-class TestClass {
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class TestClass : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
 }
